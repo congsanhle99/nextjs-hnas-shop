6627a2c2db12277e93ecdad525024fe73b96d2bd
CongSanhLe <congsanhle1999@gmail.com>
2023-02-20 01:00:53 +0700

    [20022023] Reset css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className={styles.red}>
         <h1>NextJs 13 down to NextJs 12</h1>
       </main>
     </>
