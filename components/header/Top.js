bb4389163368b81602af69279826f208a9f85f45
CongSanhLe <congsanhle1999@gmail.com>
2023-02-21 16:36:03 +0700

    [21022023] menu - menu user

--- components/header/Top.js
+++ components/header/Top.js
@@ -1,46 +1,66 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import { MdSecurity } from "react-icons/md";
 import { BsSuitHeart } from "react-icons/bs";
 import { RiAccountPinCircleLine, RiArrowDropDownFill } from "react-icons/ri";
 import Link from "next/link";
+import UserMenu from "./UserMenu";
 
 const Top = () => {
+  const [loggedIn, setLoggedIn] = useState(true);
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className={styles.top}>
       <div className={styles.top__container}>
         <div className=""></div>
         <ul className={styles.top__list}>
-          <li>
+          <li className={styles.li}>
             <img
               src="https://www.seekpng.com/png/full/47-477206_vietnam-war-flag-of-vietnam-south-vietnam-indochina.png"
               alt="flag"
             />
             <span>VietNam / vnd</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <MdSecurity />
             <span>Buyer Protection</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <span>Customer Service</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <span>Helps</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <BsSuitHeart />
             <Link href="/profile/whishlist">
               <span>Whishlist</span>
             </Link>
           </li>
-          <li>
-            <div className={styles.flex}>
-              <RiAccountPinCircleLine />
-              <span>Account</span>
-              <RiArrowDropDownFill />
-            </div>
+          <li className={styles.li} onMouseOver={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
+            {loggedIn ? (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <img
+                    src="https://e7.pngegg.com/pngimages/1008/377/png-clipart-computer-icons-avatar-user-profile-avatar-heroes-black-hair-thumbnail.png"
+                    alt="avatar"
+                  />
+                  <span>Dannn</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            ) : (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <RiAccountPinCircleLine />
+                  <span>Account</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            )}
+            {visible && <UserMenu log={loggedIn} />}
           </li>
         </ul>
       </div>
