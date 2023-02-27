551b4256823be14ab1c31422c8a7152f025ddfb1
CongSanhLe <congsanhle1999@gmail.com>
2023-02-27 14:52:50 +0700

    [27022023] jwt

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,9 @@
 import nc from "next-connect";
 import { connectDb } from "../../../utils/db";
 import { validationEmail } from "../../../utils/validation";
+import User from "../../../models/User";
+import bcrypt from "bcrypt";
+import { createActivationToken } from "../../../utils/token";
 const handler = nc();
 
 handler.post(async (req, res) => {
@@ -15,6 +18,23 @@ handler.post(async (req, res) => {
     if (!validationEmail(email)) {
       return res.status(400).json({ message: "Invalid Email!" });
     }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "This email is already exists!" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters!" });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: cryptedPassword });
+    const addUser = await newUser.save();
+    const activation_token = createActivationToken({
+      id: addUser._id.toString(),
+    });
+    console.log("activation_token", activation_token);
+    res.send(activation_token);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
