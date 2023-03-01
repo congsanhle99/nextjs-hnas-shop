16370f6b8311e6c7db70a30368576591e8a388c3
CongSanhLe <congsanhle1999@gmail.com>
2023-03-01 12:05:12 +0700

    [01032023] home page structure

---
+++ components/home/main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Main = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>header</div>
+      <div className={styles.menu}>menu</div>
+      <div className={styles.swiper}>swiper</div>
+      <div className={styles.offers}>offers</div>
+      <div className={styles.user}>user</div>
+    </div>
+  );
+};
+
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.scss";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Main from "../components/home/main/Main";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -11,6 +12,11 @@ export default function Home() {
   return (
     <>
       <Header></Header>
+      <div className={styles.home}>
+        <div className={styles.container}>
+          <Main />
+        </div>
+      </div>
       <Footer></Footer>
     </>
   );
