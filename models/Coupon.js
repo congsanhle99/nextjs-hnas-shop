010f53273db55681e5c8f0ca16d1e5f6c781d4b8
CongSanhLe <congsanhle1999@gmail.com>
2023-04-05 16:38:09 +0700

    [1213] Coupon Model

---
+++ models/Coupon.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema;
+
+const couponSchema = new mongoose.Schema(
+  {
+    coupon: {
+      type: String,
+      trim: true,
+      unique: true,
+      uppercase: true,
+      required: true,
+      minLength: 8,
+      maxLength: 16,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const Coupon = mongoose.models.Coupon || mongoose.model("Coupon", couponSchema);
+
+export default Coupon;
