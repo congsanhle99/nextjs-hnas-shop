666664e39142843d2670c9d83217919c7be08023
CongSanhLe <congsanhle1999@gmail.com>
2023-04-07 00:06:27 +0700

    [1302] Order Header n Products list

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
 import Header from "../../components/header/Header";
 import Order from "../../models/Order";
 import styles from "../../styles/order.module.scss";
@@ -7,6 +9,69 @@ const order = ({ order }) => {
   return (
     <>
       <Header />
+
+      <div className={styles.order}>
+        <div className={styles.container}>
+          <div className={styles.order__infos}>
+            <div className={styles.order__header}>
+              <div className={styles.order__header_head}>
+                Home <IoIosArrowForward /> Orders <IoIosArrowForward /> ID {order._id}
+              </div>
+              <div className={styles.order__header_status}>
+                Payment Status:
+                {order.isPaid ? (
+                  <img src="../../../images/paid/verified.png" alt="paid" />
+                ) : (
+                  <img src="../../../images/paid/unverified.png" alt="paid" />
+                )}
+              </div>
+              <div className={styles.order__header_status}>
+                Order Status:{" "}
+                <span
+                  className={
+                    order.status == "Not Processed"
+                      ? styles.not_processed
+                      : order.status == "Processing"
+                      ? styles.processing
+                      : order.status == "Dispatched"
+                      ? styles.dispatched
+                      : order.status == "Cancel"
+                      ? styles.cancel
+                      : order.status == "Completed"
+                      ? styles.completed
+                      : ""
+                  }
+                >
+                  {order.status}
+                </span>
+              </div>
+            </div>
+
+            <div className={styles.order__products}>
+              {order.products.map((product) => (
+                <div className={styles.product} key={product._id}>
+                  <div className={styles.product__img}>
+                    <img src={product.image} alt={product.name} />
+                  </div>
+                  <div className={styles.product__infos}>
+                    <h1 className={styles.product__infos_name}>
+                      {product.name.length > 30 ? `${product.name.substring(0, 30)}...` : product.name}
+                    </h1>
+                    <div className={styles.product__infos_style}>
+                      <img src={product.color.image} alt="" /> / {product.size}
+                    </div>
+                    <div className={styles.product__infos_priceQty}>
+                      {product.price}$ x {product.qty}
+                    </div>
+                    <div className={styles.product__infos_total}>{product.price * product.qty}$</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={styles.order__actions}></div>
+        </div>
+      </div>
     </>
   );
 };
