f17c8231e1d79dbd7c0861107ff70e755573d9ae
CongSanhLe <congsanhle1999@gmail.com>
2023-04-11 16:03:58 +0700

    [1701] Ad Categories Page Setup

---
+++ pages/admin/dashboard/categories.js
@@ -0,0 +1,29 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
+import Create from "../../../components/admin/categories/Create";
+import Layout from "../../../components/admin/layout";
+import Category from "../../../models/Category";
+import db from "../../../utils/db";
+
+const categories = ({ categories }) => {
+  const [data, setData] = useState(categories);
+  return (
+    <Layout>
+      <div className="">
+        <Create setCategories={setData} />
+      </div>
+    </Layout>
+  );
+};
+
+export default categories;
+
+export async function getServerSideProps(context) {
+  db.connectDb();
+  const categories = await Category.find({}).sort({ updatedAt: -1 }).lean();
+  return {
+    props: {
+      categories: JSON.parse(JSON.stringify(categories)),
+    },
+  };
+}
