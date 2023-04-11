2a0ed978030347b11738c861cb353cc974c0381a
CongSanhLe <congsanhle1999@gmail.com>
2023-04-11 16:37:54 +0700

    [1702] Ad Categories Create Category

--- components/admin/categories/Create.js
+++ components/admin/categories/Create.js
@@ -1,8 +1,45 @@
-import React from "react";
+import { Form, Formik } from "formik";
+import React, { useState } from "react";
+import * as Yup from "yup";
+import AdminInput from "../../inputs/adminInput";
 import styles from "./styles.module.scss";
 
 const Create = () => {
-  return <div>Create</div>;
+  const [name, setName] = useState("");
+  const validate = Yup.object({
+    name: Yup.string(),
+  });
+
+  const submitHandler = async () => {};
+
+  return (
+    <>
+      <Formik
+        enableReinitialize
+        initialValues={{ name }}
+        validationSchema={validate}
+        onSubmit={() => {
+          submitHandler();
+        }}
+      >
+        {(formik) => (
+          <Form>
+            <div className={styles.header}>Create a Category</div>
+            <AdminInput
+              type="text"
+              label="Name"
+              name="name"
+              placeholder="Category name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" className={`${styles.btn} ${styles.btn__primary}`}>
+              <span>Add Category</span>
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 };
 
 export default Create;
---
+++ components/inputs/adminInput/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const AdminInput = () => {
+  return <div>AdminInput</div>;
+};
+
+export default AdminInput;
