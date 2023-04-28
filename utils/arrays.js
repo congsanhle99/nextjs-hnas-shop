94c986fd47ebd4399ed2b140fbe6e985c8a17004
CongSanhLe <congsanhle1999@gmail.com>
2023-04-28 16:28:59 +0700

    [2701] Getting the data

--- utils/arrays.js
+++ utils/arrays.js
@@ -14,3 +14,22 @@ export const compareArrays = (array1, array2) => {
 
   return array2.every((object) => array1.has(neww(object)));
 };
+
+// Filter Product details -> get value of "Style"
+export const filterArray = (array, property) => {
+  return array
+    .filter((item) => item.name == property)
+    .map((s) => {
+      return s.value;
+    });
+};
+
+// remove duplicate
+export const removeDuplicates = (array) => {
+  return [...new Set(array)];
+};
+
+// Shuffle array
+export const randomize = (array) => {
+  return [...array].sort(() => 0.5 - Math.random());
+};
