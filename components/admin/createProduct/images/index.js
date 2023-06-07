1566da0f0ac7fa7abd63832a86d78df6436e5a38
CongSanhLe <congsanhle1999@gmail.com>
2023-06-07 16:52:41 +0700

    FIX Admin add img length

--- components/admin/createProduct/images/index.js
+++ components/admin/createProduct/images/index.js
@@ -14,7 +14,7 @@ const Images = ({ name, header, text, images, setImages, setColorImage, ...props
   const handleImages = (e) => {
     let files = Array.from(e.target.files);
     files.forEach((img, idx) => {
-      if (images.length == 6) {
+      if (images.length > 6 || idx > 5) {
         dispatch(
           showDialog({
             header: "Maximum 6 images are allowed",
@@ -142,8 +142,8 @@ const Images = ({ name, header, text, images, setImages, setColorImage, ...props
       </div>
       <button
         type="reset"
-        disabled={images.length == 6}
-        style={{ opacity: `${images.length == 6 && "0.5"}` }}
+        disabled={images.length >= 6}
+        style={{ opacity: `${images.length >= 6 ? "0.5" : ""}` }}
         onClick={() => fileInput.current.click()}
         className={`${styles.btn} ${styles.btn__primary}`}
       >
